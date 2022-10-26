e98f256db5717083719582f5a665b1212c881f41
Andrew Masse <amasse@gmail.com>
2022-10-26 11:04:31 -0400

    First draft complete

--- src/Configure-AADTenantBaseline.ps1
+++ src/Configure-AADTenantBaseline.ps1
@@ -1,2 +1,26 @@
 #Placeholder
 Install-Module Microsoft.Graph
+
+#region functions
+
+#endregion
+
+#region Emergency-Access
+#Create Emergency Access accounts
+#endregion
+
+#region User-Settings
+
+#endregion
+
+#region Collaboration-Settings
+
+#endregion
+
+#region MLZ-Groups
+
+#endregion
+
+#region 
+
+#endregion
